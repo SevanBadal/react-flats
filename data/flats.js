649ea70bb55ce0db279e0f8a29f95c6d6e544533
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-10 18:03:13 -0400

    local regional data

---
+++ data/flats.js
@@ -0,0 +1,52 @@
+const flats = [
+  {
+    name: "Carbon Terrace",
+    imageUrl: "https://images1.apartments.com/i2/4SEbL-DPqcOJSL_MYhr7fGRi5TegkEPoQIAVoK_HMNw/110/1185-south-burlington-vt-primary-photo.jpg",
+    price: 1500,
+    priceCurrency: "EUR",
+    lat: 44.436870,
+    lng: -73.210910
+  },
+  {
+    name: "5 Corners",
+    imageUrl: "https://images1.apartments.com/i2/jN5gTys6vN7r7ELDnfY1TNHY0uwWxiXEWH29g10tABQ/111/5-corners-essex-junction-vt-primary-photo.jpg",
+    price: 1700,
+    priceCurrency: "USD",
+    lat: 44.490310,
+    lng: -73.111890
+  },
+  {
+    name: "Village at Autumn Pond",
+    imageUrl: "https://images1.apartments.com/i2/UqoJ1tpuqlsmMba6BTY24cYlEO9OpTytJ8gWvDZGpSI/110/village-at-autumn-pond-essex-junction-vt-pool-and-clubhouse-at-night.jpg",
+    price: 1800,
+    priceCurrency: "USD",
+    lat: 44.501990,
+    lng: -73.108320
+  },
+  {
+    name: "Keen's Crossing",
+    imageUrl: "https://images1.apartments.com/i2/q2wBROx4MojsQMNzoe7NFVDPwpE8bbszLoptpIUdwvA/110/keens-crossing-winooski-vt-primary-photo.jpg",
+    price: 1200,
+    priceCurrency: "USD",
+    lat: 44.490440,
+    lng: -73.183600
+  },
+  {
+    name: "Severance Corners Village Center",
+    imageUrl: "https://images1.apartments.com/i2/yJlDcQVwqp3R60N0DuIJUei4D6-MoR6ftS6tB0w8e3k/110/severance-corners-village-center-colchester-vt-aerial-view-wiht-lake-champlain.jpg",
+    price: 1500,
+    priceCurrency: "USD",
+    lat: 44.528640,
+    lng: -73.174100
+  },
+  {
+    name: "Bayberry Commons",
+    imageUrl: "https://images1.apartments.com/i2/TGBroTkUrx4C1IgOhkKq3hIPspLvI9gOC2967Ov2VQs/110/bayberry-commons-apartments-burlington-vt-grove-street-entrance.jpg",
+    price: 1600,
+    priceCurrency: "USD",
+    lat: 44.483330,
+    lng: -73.183700
+  }
+];
+
+export default flats;
